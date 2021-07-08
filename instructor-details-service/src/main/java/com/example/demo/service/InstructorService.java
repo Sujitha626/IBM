d14b2eb8a82b8283c555c866c95d2e824dc7a75e
Sujitha626 <85486124+Sujitha626@users.noreply.github.com>
2021-07-08 18:09:11 +0530

    Add files via upload

--- instructor-details-service/src/main/java/com/example/demo/service/InstructorService.java
+++ instructor-details-service/src/main/java/com/example/demo/service/InstructorService.java
@@ -1,9 +1,17 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+
 import com.example.demo.entity.Instructor;
+import com.example.demo.entity.InstructorDetail;
+
 
 public interface InstructorService {
-	
-	public Instructor createInstructor(Instructor instructor);
 
+	public Instructor createInstructor(Instructor instructor);
+	public List<Instructor> getAllInstructor();
+	public Instructor getInstructorById(Integer id);
+	public String deleteInstructorById(Integer id);
+	public Instructor updateInstructorById(Integer id, Instructor instructor, InstructorDetail instructorDetail );
 }
